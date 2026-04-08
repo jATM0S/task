ca46d1c678a3a25e555d2d51fd078edc1618443b
jATM0S <bks.mhr8@gmail.com>
2026-04-09 03:36:56 +0545

    fix: logic fixes

--- app/ui-2/_components/images-section.tsx
+++ app/ui-2/_components/images-section.tsx
@@ -26,9 +26,17 @@ const ImagesSection = ({
 }) => {
   const imgRef = useRef<HTMLDivElement | null>(null);
   const prevIdRef = useRef<string>(id);
+
+  const whereToAnimateTo = () => {
+    return getDirection(prevIdRef.current, activeId);
+  };
+
+  const whereToAnimateFrom = () => {
+    return getDirection(activeId, prevIdRef.current);
+  };
+
   const getDirection = (from: string, to: string) => {
     const fromIndex = ids.indexOf(from);
-    prevIdRef.current = activeId;
     const toIndex = ids.indexOf(to);
 
     if (toIndex > fromIndex) return "right";
@@ -36,19 +44,21 @@ const ImagesSection = ({
     return "same";
   };
   const clickedOffFromYourParentDiv = () => {
-    return prevIdRef.current !== id;
+    return prevIdRef.current == id;
   };
 
   useEffect(() => {
     // we got two functions get direction and clicked off from your parent div. if clickedOffFromYourParentDiv is true, we need to get the direction where it was clicked off. Then animate the images to that direction. Similary if the clickedOffFromYourParentDiv is false and active element is your parent div, we need to animate the images to the center. The images should animate to the right if the direction is right, to the left if the direction is left.
 
     // clicked off from parent div then get the direction and animate images to that direction == animate to the direction
-    // !clicked off from your parent div and active element is your parnt div == animate to the center
+    // !clicked off from your parent div which (means your parent div is the active div) then get the direction of previous element == animate to the center from that direction
 
     console.log(id);
-    ``;
-    console.log(id, getDirection(activeId, prevIdRef.current));
-    console.log(id, clickedOffFromYourParentDiv());
+    // console.log(id, getDirection(activeId, prevIdRef.current));
+    console.log("clicked off your parent div:", clickedOffFromYourParentDiv());
+    console.log("where to animate to:", whereToAnimateTo());
+    console.log("where to animate from:", whereToAnimateFrom());
+    prevIdRef.current = activeId;
   }, [activeId, getDirection]);
 
   return (
